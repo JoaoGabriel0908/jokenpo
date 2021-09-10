e2dbd93ec562847baf596bd90d67fb97f2b05ea9
JOÃO GABRIEL COSTA FLÔR <21276663@CFP127ALUNO.LOCAL>
2021-09-10 16:51:07 -0300

    Atualizado jogo Jokenpo

--- src/jokenpo/desafioPedraPapelTesoura.java
+++ src/jokenpo/desafioPedraPapelTesoura.java
@@ -10,14 +10,15 @@ public class desafioPedraPapelTesoura {
 		Random sorteio = new Random();
 		Scanner leitor = new Scanner(System.in);
 		
-		int valorSorteado = sorteio.nextInt(3) + 1;
 		int pedra=1;
 		int papel=2;
 		int tesoura=3;
 		int maior;
-		int voceGanhou = 0;
-		int computadorGanhou = 0;
-		int partidas = 2;
+		int voceGanhou = 1 ;
+		int computadorGanhou = 1 ;
+		int partidas = 1;
+		int empate = 1;
+		int rodadas = 1;
 		
 		System.out.println("--------------");
 		System.out.println("J O K E N P O ");
@@ -31,52 +32,81 @@ public class desafioPedraPapelTesoura {
 		System.out.println("--------------");
 		System.out.println();
 		
-		System.out.print("Qual a sua escolha? ");
-		int ppt = leitor.nextInt();
+		while(partidas <= 1) {	
 		
-		System.out.println("Escolha do Comptador: " + valorSorteado);
-		leitor.close();
+			System.out.print("Quantas partidas você quer jogar? " );
+			partidas = leitor.nextInt();
+			if (partidas % 2 == 1) {
 		
+		while (voceGanhou < partidas && computadorGanhou < partidas ) {
+			System.out.println();
+			System.out.print("Qual a sua escolha? ");
+			int ppt = leitor.nextInt();
+			
+			int valorSorteado = sorteio.nextInt(3) + 1;
+			System.out.println("Escolha do Comptador: " + valorSorteado);
+			System.out.println();
 		
 			if (ppt == 1 && valorSorteado == 3 ) {
 				System.out.println("Você escolheu pedra");
 				System.out.println("O computador escolheu Tesoura");
-				System.out.println("Você venceu");
+				System.out.println(" Você venceu " + voceGanhou++);
 				
 			}else if (ppt == 1 && valorSorteado == 2) {
 				System.out.println("Você escolheu pedra");
 				System.out.println("O computador escolheu papel");
-				System.out.println("Computador ganhou");
+				System.out.println(" Computador ganhou " + computadorGanhou++);
 				
 			}else if (ppt == 2 && valorSorteado == 1) {
 				System.out.println("Você escolheu papel");
 				System.out.println("O computador escolheu pedra");
-				System.out.println("Você venceu");
+				System.out.println(" Você venceu " + voceGanhou++);
 				
 			}else if (ppt == 2 && valorSorteado == 3) {
 				System.out.println("Você escolheu papel");
 				System.out.println("O computador escolheu tesoura");
-				System.out.println("Computador ganhou");
+				System.out.println(" Computador ganhou " + computadorGanhou++);
 				
 			}else if (ppt == 3 && valorSorteado == 1) {
 				System.out.println("Você escolheu tesoura");
 				System.out.println("O computador escolheu pedra");
-				System.out.println("Você venceu");	
+				System.out.println(" Você venceu " + voceGanhou++);	
 				
 			}else if (ppt == 3 && valorSorteado == 2) {
 				System.out.println("Você escolheu tesoura");
 				System.out.println("O computador escolheu papel");
-				System.out.println("Computador ganhou");	
+				System.out.println("Computador ganhou" + computadorGanhou++);	
 			} else {
 				System.out.println("Você escolheu: " + ppt);
 				System.out.println("O computador escolheu: " + valorSorteado);
-				System.out.println("EMPATE");
+				System.out.println(" EMPATE " + empate++);
+			
 			}
-
-
-
+			if (empate <= partidas) {
+				partidas--;
+			} else {
+				
+			}
+		
+			
+			
+		
+		}
+		
+			leitor.close();
+		}  else  {
+			System.out.println("Para haver um ganhador, este número precisa ser ímpar. Selecione novamente: ");
+			partidas++;
+		}
+		
+			
+		}
 	}
 }
+		
+	
+	
+
 		
 
 	
